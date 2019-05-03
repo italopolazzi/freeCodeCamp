56f6f0c957dccebed2998d1a840deb715bd172a6
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-03 19:45:50 -0300

    feat: timeline added

--- tributePage/js/nav-fixed-controller.js
+++ tributePage/js/nav-fixed-controller.js
@@ -1,23 +1,11 @@
 export default () => {
-    // alert("BK")
     const navElement = document.getElementById('nav');
-    // console.log(navElement);
-
     window.addEventListener('scroll', (event) => {
         const navHeight = navElement.offsetHeight;
-
-        // console.log(navHeight);
-        // console.log(window.pageYOffset);
-
         if (window.pageYOffset >= navHeight) {
-            navElement.style.position = 'fixed';
-            // console.log("fixou");
-            
+            navElement.setAttribute('class', 'nav-fixed');
         } else {
-            navElement.style.position = 'initial';
-            // console.log("não");
+            navElement.removeAttribute('class', 'nav-fixed');
         }
-
     })
-
 }
