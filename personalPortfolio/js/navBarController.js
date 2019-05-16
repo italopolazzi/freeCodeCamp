8cc36b169fdedcb5fb8545448b8dbc4fe02db664
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-16 04:57:49 -0300

    fix: change the nav_button to be located exactly under of the navbar

--- personalPortfolio/js/navBarController.js
+++ personalPortfolio/js/navBarController.js
@@ -1,6 +1,4 @@
 export default () => {
-    // const nav = document.getElementById("navbar").style.transform = 'translateY(-100%)'
-    const nav_button = document.getElementById("nav_button")
-    nav_button.style.display = 'flex'
-}
 
+
+}
