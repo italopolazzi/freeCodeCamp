82cfebed0f72305d65d8832d526e58a5850ffd0d
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-16 05:59:30 -0300

    add: navbar visibility controlled by either scrollY and nav_button

--- personalPortfolio/js/navBarController.js
+++ personalPortfolio/js/navBarController.js
@@ -1,4 +1,22 @@
 export default () => {
 
+    const nav = document.getElementById("navbar")
+    const nav_height = Number.parseInt(getComputedStyle(nav).height)
 
+    const nav_button = document.getElementById("nav_button")
+    const nav_button_transform = getComputedStyle(nav_button).transform
+
+    const switchNavBar = (test) => {
+        const pos = ['translateY(-100%)', 'initial'];
+        const neg = ['translate(0)', nav_button_transform];
+        [nav.style.transform, nav_button.style.transform] = test && true ? pos : neg
+    }
+
+    window.addEventListener('scroll', () => {
+        switchNavBar(window.scrollY > nav_height)
+    })
+
+    nav_button.addEventListener('click', () => {
+        switchNavBar(false)
+    })
 }
