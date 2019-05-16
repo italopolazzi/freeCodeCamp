0460bb876e34b5385621d6d48b5a19500f386438
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-16 04:39:00 -0300

    add: navbar controller using just css

--- personalPortfolio/js/index.js
+++ personalPortfolio/js/index.js
@@ -1,9 +1,11 @@
 import darkMode from './darkMode.js'
 import iframeAdjusts from './iframeAdjusts.js'
 // import iframeUpAndDown from './iframeUpAndDown.js'
+import navBarController from './navBarController.js'
 
 (() => {
     darkMode()
     iframeAdjusts()
     // iframeUpAndDown()
+    navBarController()
 })()
---
+++ personalPortfolio/js/navBarController.js
@@ -0,0 +1,6 @@
+export default () => {
+    // const nav = document.getElementById("navbar").style.transform = 'translateY(-100%)'
+    const nav_button = document.getElementById("nav_button")
+    nav_button.style.display = 'flex'
+}
+
