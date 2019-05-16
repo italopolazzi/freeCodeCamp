5db9762c25f933abdbcfc7a4ed3725652affae4f
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-16 02:34:32 -0300

    add: projects-tiles footer to put in information

--- personalPortfolio/js/iframeAdjusts.js
+++ personalPortfolio/js/iframeAdjusts.js
@@ -1,14 +1,15 @@
 export default () => {
     const iframes = document.querySelectorAll('#projects iframe')
-    // console.log(iframes)
 
     iframes.forEach(iframe => {
-        const frame_document = iframe.contentWindow.document
+        const frame_window = iframe.contentWindow
+        const frame_document = frame_window.document
+        
         const frame_style = frame_document.createElement('style')
         frame_style.type = 'text/css'
         frame_style.rel = 'stylesheet'
         frame_style.innerHTML = 'body,nav,main{overflow: hidden}'
-        console.log(frame_style)
+        
         frame_document.head.appendChild(frame_style)
     })
 
