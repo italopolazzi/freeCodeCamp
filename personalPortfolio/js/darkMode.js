9e1da7d70faee983ec0bad8a79142381214c3650
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-19 21:52:22 -0300

    add: try-catch to work without Storage undefined erros

--- personalPortfolio/js/darkMode.js
+++ personalPortfolio/js/darkMode.js
@@ -4,18 +4,18 @@ export default () => {
     const dark_element_id = 'dark-mode'
 
     const storage = window.localStorage;
-    const saved_as_dark = storage.getItem(dark_flag)
 
     const dark_mode_checkbox = document.getElementById(dark_element_id);
     const body_classes = document.body.classList;
 
     const alternate = test => {
+        try {
+            storage.setItem(dark_flag, test)
+        } catch (error) {
+            
+        }
         test ? addDarkFlag() : removeDarkFlag()
-        storage.setItem(dark_flag, test)
     }
-
-    // dark_mode_checkbox.addEventListener('load', )
-
 
     const addDarkFlag = () => {
         body_classes.add(dark_flag)
@@ -24,7 +24,13 @@ export default () => {
         body_classes.remove(dark_flag)
     }
     const init = () => {
-        const saved_as_dark_parsed = JSON.parse(saved_as_dark)
+        let saved_as_dark, saved_as_dark_parsed;
+        try {
+            saved_as_dark = storage.getItem(dark_flag)
+            saved_as_dark_parsed = JSON.parse(saved_as_dark)
+        } catch (error) {
+            
+        }
         dark_mode_checkbox.checked = saved_as_dark ? saved_as_dark_parsed : saved_as_dark_parsed || document.body.classList.contains(dark_flag)
         alternate(dark_mode_checkbox.checked)
     }
