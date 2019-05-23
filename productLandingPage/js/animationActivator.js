945afbea24eaf7b575ffd71e2af7f1e71bc654ae
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-23 07:39:35 -0300

    add: pricing section styles

--- productLandingPage/js/animationActivator.js
+++ productLandingPage/js/animationActivator.js
@@ -2,18 +2,16 @@ export default () => {
     const activator_flag = 'data-animation-activator'
     const object_flag = 'data-animation-object'
     const class_flag = 'animation-activated'
-    const min_distance = 10
+    const min_distance = 50
 
     const getActivators = () => {
         return document.querySelectorAll(`*[${activator_flag}]`)
     }
-    
+
     const getObjects = () => {
 
     }
-
-    const activators = getActivators()
-
+    
     window.addEventListener('scroll', () => {
         getActivators().forEach(activator => {
             if (activator.getBoundingClientRect().y < min_distance) {
