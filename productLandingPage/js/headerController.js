651cac7836a21602541fc2d483982abb7e75c6b0
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-28 21:07:51 -0300

    add: welcome letters animations

--- productLandingPage/js/headerController.js
+++ productLandingPage/js/headerController.js
@@ -9,33 +9,33 @@ const header = document.querySelector("#header")
 const nav = document.querySelector("#nav-bar")
 const options = document.querySelector("#options")
 
-const nav_controller = document.querySelector("#nav-bar-controller")
-const options_controller = document.querySelector("#options-controller")
-
 export default () => {
     window.addEventListener('scroll', (e) => {
         console.log(scroll)
-        if(scrollY > 5){
+        if (scrollY > 5) {
             header.classList.add('scroll-activated')
         } else {
             header.classList.remove('scroll-activated')
         }
         functions.removeTheClassForAll(header, nav, options)
     })
-    
+
     window.addEventListener('click', (e) => {
         console.log(elementHasGrandParent(e.target))
 
 
 
-        if (e.target === nav_controller) {
+        if (e.target.closest("#nav-bar-controller")) {
             functions.switchClassActivated(header, nav)
             options.classList.remove(flags.activated_class_flag)
 
-        } else if (e.target === options_controller) {
+        } else if (e.target.closest("#options-controller")) {
             functions.switchClassActivated(header, options)
             nav.classList.remove(flags.activated_class_flag)
 
+        } else {
+
+            functions.removeTheClassForAll(header, nav, options)
         }
     })
 }
