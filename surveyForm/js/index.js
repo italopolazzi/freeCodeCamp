f0c9d3011c3ad592f6231344f8e602ad7c8ecf00
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-05-31 23:40:47 -0300

    add: buttons max and min for numbers inputs

--- surveyForm/js/index.js
+++ surveyForm/js/index.js
@@ -1,5 +1,19 @@
 import customForm from './customForm/customForm.js'
 
+const buttonMaxMin = (event, attribute) => {
+    const input_number = event.target.parentNode.querySelector('input[type=number]')
+    const input_attribute_value = input_number.getAttribute(attribute)
+    input_number.value = input_attribute_value
+}
+
 (() => {
     customForm()
+
+
+    const max_number_buttons = document.querySelectorAll('.button.max')
+    const min_number_buttons = document.querySelectorAll('.button.min')
+
+    max_number_buttons.forEach(btn => btn.addEventListener('click', e => buttonMaxMin(e, 'max')))
+    min_number_buttons.forEach(btn => btn.addEventListener('click', e => buttonMaxMin(e, 'min')))
+    
 })()
